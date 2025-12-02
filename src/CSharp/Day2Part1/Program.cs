6e566bd6b46d7f48bd308568d929833911a70164
Arthri <41360489+a@users.noreply.github.com>
2025-12-02 05:28:44 +0000

    Solve day 2

---
+++ src/CSharp/Day2Part1/Program.cs
@@ -0,0 +1,41 @@
+﻿var sum = 0ul;
+var line = Console.ReadLine();
+
+foreach (var range in line.AsSpan().Split(','))
+{
+    var rangeSpan = line.AsSpan(range);
+    var enumerator = rangeSpan.Split('-');
+    enumerator.MoveNext();
+    var left = ulong.Parse(rangeSpan[enumerator.Current]);
+    enumerator.MoveNext();
+    var right = ulong.Parse(rangeSpan[enumerator.Current]);
+
+    for (ulong i = left; i <= right; i++)
+    {
+        if (IsInvalid(i))
+        {
+            sum += i;
+        }
+    }
+}
+
+Console.WriteLine(sum);
+
+ulong GetPlaces(ulong num)
+{
+    return (ulong)Math.Ceiling(Math.Log10(num));
+}
+
+bool IsInvalid(ulong num)
+{
+    var places = GetPlaces(num);
+    var (div, rem) = ulong.DivRem(places, 2);
+    
+    if (rem != 0)
+    {
+        return false;
+    }
+
+    var mask = (ulong)Math.Pow(10, div);
+    return num / mask == num % mask;
+}
---
+++ src/CSharp/Day2Part2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Numerics;
+
+var sum = 0ul;
+var line = Console.ReadLine();
+
+foreach (var range in line.AsSpan().Split(','))
+{
+    var rangeSpan = line.AsSpan(range);
+    var enumerator = rangeSpan.Split('-');
+    enumerator.MoveNext();
+    var left = ulong.Parse(rangeSpan[enumerator.Current]);
+    enumerator.MoveNext();
+    var right = ulong.Parse(rangeSpan[enumerator.Current]);
+
+    for (ulong i = left; i <= right; i++)
+    {
+        if (IsInvalid(i))
+        {
+            sum += i;
+        }
+    }
+}
+
+Console.WriteLine(sum);
+
+ulong GetPlaces(ulong num)
+{
+    return (ulong)Math.Ceiling(Math.Log10(num));
+}
+
+bool IsInvalid(ulong num)
+{
+    var places = GetPlaces(num);
+    for (ulong i = 2; i <= places; i++)
+    {
+        if (HasPattern(num, places, i))
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool HasPattern(ulong num, ulong places, ulong fold)
+{
+    var (div, rem) = ulong.DivRem(places, fold);
+
+    if (rem != 0)
+    {
+        return false;
+    }
+
+    var mask = (ulong)Math.Pow(10, div);
+    for ((num, var pattern) = ulong.DivRem(num, mask); num > 0; num /= mask)
+    {
+        if (num % mask != pattern)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
