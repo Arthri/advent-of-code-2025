7acda089bb0ecf001352116a7084ce760e550288
Arthri <41360489+a@users.noreply.github.com>
2025-12-02 03:42:36 +0000

    Solve Day1Part2

---
+++ src/CSharp/Day1Part2/Program.cs
@@ -0,0 +1,18 @@
+﻿string? line;
+int dialNumber = 50;
+int zeroCount = 0;
+
+while (!string.IsNullOrWhiteSpace(line = Console.ReadLine()))
+{
+    int spin = int.Parse(line.AsSpan(1));
+    int prevDial = dialNumber;
+    dialNumber = dialNumber + spin * (line[0] == 'R' ? 1 : -1);
+    zeroCount += Math.Abs(dialNumber / 100);
+    if (prevDial > 0 && dialNumber <= 0)
+    {
+        zeroCount += 1;
+    }
+    dialNumber = (100 + dialNumber % 100) % 100;
+}
+
+Console.WriteLine(zeroCount);
