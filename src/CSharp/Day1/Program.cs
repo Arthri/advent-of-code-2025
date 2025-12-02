d8992f8447689b19407d35f7af1d4450dde569ab
Arthri <41360489+a@users.noreply.github.com>
2025-12-02 03:08:32 +0000

    Solve day 1 part 1

--- src/CSharp/Day1/Program.cs
+++ src/CSharp/Day1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? line;
+int dialNumber = 50;
+int zeroCount = 0;
+
+while (!string.IsNullOrWhiteSpace(line = Console.ReadLine()))
+{
+    int spin = int.Parse(line.AsSpan(1));
+    dialNumber = (100 + (dialNumber + spin * (line[0] == 'R' ? 1 : -1)) % 100) % 100;
+    if (dialNumber == 0)
+    {
+        zeroCount++;
+    }
+}
+
+Console.WriteLine(zeroCount);
