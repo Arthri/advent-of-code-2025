73ea9795fe838657bc4df52ca3f75f60a49f7d00
Arthri <41360489+a@users.noreply.github.com>
2025-12-03 05:31:42 +0000

    Solve day 3

---
+++ src/CSharp/Day3Part1/Program.cs
@@ -0,0 +1,37 @@
+﻿var sum = 0;
+Span<char> digits = stackalloc char[3];
+
+while (Console.ReadLine() is var line && !string.IsNullOrWhiteSpace(line))
+{
+    digits.Fill('\0');
+
+    int maxIndex = -1;
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (line[i] > digits[1])
+        {
+            digits[0] = digits[1];
+            digits[1] = line[i];
+            maxIndex = i;
+        }
+    }
+
+    for (int i = maxIndex + 1; i < line.Length; i++)
+    {
+        if (line[i] > digits[2])
+        {
+            digits[2] = line[i];
+            maxIndex = i;
+        }
+    }
+
+    if (digits[2] == '\0')
+    {
+        digits[2] = digits[1];
+        digits[1] = digits[0];
+    }
+
+    sum += (digits[1] - '0') * 10 + digits[2] - '0';
+}
+
+Console.WriteLine(sum);
---
+++ src/CSharp/Day3Part2/Program.cs
@@ -0,0 +1,39 @@
+﻿const int NUM_BATTERIES = 12;
+var sum = 0ul;
+Span<char> digits = stackalloc char[NUM_BATTERIES];
+
+while (Console.ReadLine() is var line && !string.IsNullOrWhiteSpace(line))
+{
+    digits.Fill('\0');
+
+    var lastIndex = -1;
+    for (int i = 0; i < NUM_BATTERIES; i++)
+    {
+        var startIndex = lastIndex + 1;
+        var (digit, index) = FindDigit(line.AsSpan(startIndex..^(NUM_BATTERIES - (1 + i))));
+        digits[i] = digit;
+        lastIndex = startIndex + index;
+    }
+
+    var mult = 1ul;
+    for (int i = digits.Length - 1; i >= 0; i--, mult *= 10)
+    {
+        sum += (ulong)(digits[i] - '0') * mult;
+    }
+}
+
+Console.WriteLine(sum);
+
+(char Digit, int Index) FindDigit(ReadOnlySpan<char> line)
+{
+    (char Digit, int Index) result = ('\0', -1);
+    for (int i = 0; i < line.Length; i++)
+    {
+        if (line[i] > result.Digit)
+        {
+            result = (line[i], i);
+        }
+    }
+
+    return result;
+}
