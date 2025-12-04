8f06c5eb9657a34e3fbaf2e0c1b8f331781d4873
Arthri <41360489+a@users.noreply.github.com>
2025-12-04 06:46:14 +0000

    Day 4 solution

---
+++ src/CSharp/Day4Part1/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+
+const int BUFFERED_LINES = 3;
+
+bool[] grid;
+Span<bool> line1;
+Span<bool> line2;
+Span<bool> line3;
+int lineLength;
+int rolls = 0;
+bool stop = false;
+
+{
+    var line = Console.ReadLine()!;
+    lineLength = line.Length;
+    grid = new bool[line.Length * BUFFERED_LINES];
+    line1 = grid.AsSpan(0, lineLength);
+    line2 = grid.AsSpan(lineLength, lineLength);
+    line3 = grid.AsSpan(lineLength * 2, lineLength);
+    for (int i = 0; i < lineLength; i++)
+    {
+        grid[lineLength + i] = line[i] == '@';
+    }
+    line = Console.ReadLine()!;
+    for (int i = 0; i < lineLength; i++)
+    {
+        grid[2 * lineLength + i] = line[i] == '@';
+    }
+}
+
+do
+{
+    for (int i = 0; i < lineLength; i++)
+    {
+        if (!line2[i])
+            continue;
+        var adjacent = 0;
+        if (i > 0 && line2[i - 1])
+            adjacent += 1;
+        if (i < lineLength - 1 && line2[i + 1])
+            adjacent += 1;
+        if (line1[i])
+            adjacent += 1;
+        if (line3[i])
+            adjacent += 1;
+        if (i > 0 && line1[i - 1])
+            adjacent += 1;
+        if (i < lineLength - 1 && line1[i + 1])
+            adjacent += 1;
+        if (i > 0 && line3[i - 1])
+            adjacent += 1;
+        if (i < lineLength - 1 && line3[i + 1])
+            adjacent += 1;
+        if (line2[i] && adjacent < 4)
+            rolls++;
+    }
+
+    var line = Console.ReadLine();
+    if (stop)
+    {
+        break;
+    }
+    if (string.IsNullOrWhiteSpace(line))
+    {
+        Array.Copy(grid, lineLength, grid, 0, lineLength * (BUFFERED_LINES - 1));
+        Array.Fill(grid, false, lineLength * 2, lineLength);
+        stop = true;
+        continue;
+    }
+
+    Array.Copy(grid, lineLength, grid, 0, lineLength * (BUFFERED_LINES - 1));
+    for (int i = 0; i < lineLength; i++)
+    {
+        grid[2 * lineLength + i] = line[i] == '@';
+    }
+}
+while (true);
+
+Console.WriteLine(rolls);
+
---
+++ src/CSharp/Day4Part2/Program.cs
@@ -0,0 +1,87 @@
+﻿List<bool[]> grid = [];
+int lineLength;
+int rolls = 0;
+
+{
+    var line = Console.ReadLine()!;
+    lineLength = line.Length;
+    grid.Add(new bool[lineLength]);
+    do
+    {
+        var lineBool = new bool[lineLength];
+        for (int i = 0; i < lineLength; i++)
+        {
+            lineBool[i] = line[i] == '@';
+        }
+        grid.Add(lineBool);
+    }
+    while (!string.IsNullOrWhiteSpace(line = Console.ReadLine()));
+    grid.Add(new bool[lineLength]);
+}
+
+var endIndex = grid.Count - 1;
+while (true)
+{
+    bool hasRemoved = false;
+    for (int i = 1; i < endIndex; i++)
+    {
+        for (int j = 0; j < lineLength; j++)
+        {
+            if (TryRemove(i, j))
+            {
+                hasRemoved = true;
+            }
+        }
+    }
+
+    if (!hasRemoved)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(rolls);
+
+bool TryRemove(int y, int x)
+{
+    if (!grid[y][x])
+        return false;
+
+    var adjacent = 0;
+
+    int yU = y - 1;
+    int yD = y + 1;
+    int xL = x - 1;
+    int xR = x + 1;
+    if (grid[yU][x])
+        adjacent++;
+    if (grid[yD][x])
+        adjacent++;
+    if (xL >= 0)
+    {
+        if (grid[yU][xL])
+            adjacent++;
+        if (grid[y][xL])
+            adjacent++;
+        if (grid[yD][xL])
+            adjacent++;
+    }
+    if (xR < lineLength)
+    {
+        if (grid[yU][xR])
+            adjacent++;
+        if (grid[y][xR])
+            adjacent++;
+        if (grid[yD][xR])
+            adjacent++;
+    }
+
+    if (adjacent < 4)
+    {
+        rolls++;
+        grid[y][x] = false;
+        return true;
+    }
+
+    return false;
+}
